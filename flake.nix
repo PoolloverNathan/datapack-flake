13d83d061471cd01e2c40c5f2e3ae538bdab6d46
PoolloverNathan <nathan.kulzer@protonmail.com>
2025-01-07 23:17:10 -0500

    feat: dimension[ type]s

--- flake.nix
+++ flake.nix
@@ -19,6 +19,7 @@
       perSystem = genAttrs (attrNames nixpkgs.legacyPackages);
       perSystemWithPkgs = f: perSystem (system: f (pkgsFor system));
       ident = types.strMatching "[0-9a-z_.-]+:[0-9a-z_/.-]+";
+      tagIdent = types.strMatching "#[0-9a-z_.-]+:[0-9a-z_/.-]+";
       splitIdent =
         s:
         let
@@ -30,6 +31,14 @@
           path = builtins.elemAt res 1;
         };
       stripNulls = import ./strip-nulls.nix;
+      mkNullOrOption =
+        type: desc:
+        mkOption {
+          type = types.nullOr type;
+          description = desc;
+          default = null;
+        };
+      mke = mkNullOrOption types.bool;
     in
     rec {
       formatter = perSystemWithPkgs (pkgs: pkgs.nixfmt-rfc-style);
@@ -273,6 +282,106 @@
                 ${p.name}."${prePost48 "functions" "function"}/${q.name}.mcfunction" = concatLines q.value.commands;
               }) (attrsToList p.value)
             ) (attrsToList config.functions)
+          );
+          config.tags = mkMerge (
+            concatMap (
+              p:
+              concatMap (
+                q:
+                map (
+                  t:
+                  let
+                    inherit (splitIdent t) namespace path;
+                  in
+                  {
+                    ${namespace}.functions.${path} = [ "${p.name}:${q.name}" ];
+                  }
+                ) q.value.addToTags
+              ) (attrsToList p.value)
+            ) (attrsToList config.functions)
+          );
+        };
+      datapackDimensionsModule =
+        { config, ... }:
+        let
+          inherit (config) format;
+          inherit (builtins) map;
+          inherit (config.pkgs.lib) concatMap concatLines attrsToList;
+          prePost48 = pre: post: if format >= 48 then post else pre;
+        in
+        {
+          options.dimensionTypes = mkOption {
+            type =
+              with types;
+              attrsOf (
+                attrsOf (submodule {
+                  options = {
+                    # Largely copied from https://minecraft.fandom.com/wiki/Custom_dimension?oldid=2356075#Dimension_type
+                    ultrawarm = mke "Whether the dimensions behaves like the nether (water evaporates and sponges dry) or not. Also lets stalactites drip lava and causes lava to spread faster and thinner.";
+                    natural = mke "When false, compasses spin randomly, and using a bed to set the respawn point or sleep, is disabled. When true, nether portals can spawn zombified piglins.";
+                    coordinate_scale = mkNullOrOption (numbers.between 1.0e-5
+                      3.0e7
+                    ) "The multiplier applied to coordinates when leaving the dimension.";
+                    has_skylight = mke "Whether the dimension has skylight or not.";
+                    has_ceiling = mke "Whether the dimension has a bedrock ceiling. Note that this is only a logical ceiling. It is unrelated with whether the dimension really has a block ceiling.";
+                    ambient_light =
+                      mkNullOrOption (numbers.between 0.0 1.0)
+                        "How much light the dimension has. When set to 0, it completely follows the light level; when set to 1, there is no ambient lighting. Precise effects need testing.";
+                    fixed_time =
+                      mkNullOrOption (ints.between 0 24000)
+                        "If this is set to an int, the time of the day is the specified value. To ensure a normal time cycle, leave the attribute undefined (i.e, do not include it).";
+                    # TODO: this is an int provider
+                    monster_spawn_light_level =
+                      mkNullOrOption (ints.between 0 15)
+                        "Maximum light required when the monster spawns. The formula of this light is `max(skyLight - 10, blockLight)` during thunderstorms, and `max(internalSkyLight, blockLight)` during other weather. ";
+                    monster_spawn_block_light_limit = mkNullOrOption (ints.between 0
+                      15
+                    ) "Maximum block light required when the monster spawns.";
+                    piglin_safe = mke "Whether Piglin and Hoglins shake and transform to zombified entities."; # modified
+                    bed_works = mke "When false, the bed blows up when trying to sleep."; # sic singular
+                    respawn_anchor_works = mke "When false, the respawn anchor blows up when trying to set spawn point."; # sic singular
+                    has_raids = mke "Whether players with the Bad Omen effect can cause a raid.";
+                    logical_height = mkNullOrOption bool "The maximum height to which chorus fruits and nether portals can bring players within this dimension. This excludes portals that were already built above the limit as they still connect normally. Cannot be greater than height."; # TODO: probably check min_y 󰥽 logical_height 󰥽 height
+                    min_y = mkNullOrOption (
+                      ints.between - 2032 2031
+                    ) "The minimum height in which blocks can exist within this dimension."; # TODO: must be a multiple of 16
+                    height = mkNullOrOption (ints.between 16
+                      4064
+                    ) "The total height in which blocks can exist within this dimension."; # TODO: multiple of 16, min_y + height - 1 󰥽 2031
+                    infiniburn = mkNullOrOption tagIdent "Fires on these blocks burns infinitely."; # sic singular verb
+                    effects = mkNullOrOption ident "Determines the dimension effect used for this dimension. Setting to overworld makes the dimension have clouds, sun, stars and moon. Setting to the nether makes the dimension have thick fog blocking that sight, similar to the nether. Setting to the end makes the dimension have dark spotted sky similar to the end, ignoring the sky and fog color.";
+                  };
+                })
+              );
+            default = { };
+          };
+          # TODO: actual dimensions
+          options.dimensions = mkOption {
+            type =
+              with types;
+              attrsOf (
+                attrsOf (submodule {
+                  options = {
+                    type = mkOption {
+                      description = "The ID of the dimension type.";
+                      type = ident;
+                    };
+                    generator = mkOption {
+                      description = "Generation settings used for that dimension.";
+                      type = attrs;
+                    };
+                  };
+                })
+              );
+            default = { };
+          };
+          config.files = mkMerge (
+            concatMap (
+              p: map (q: { ${p.name}."dimension_type/${q.name}.json" = q.value; }) (attrsToList p.value)
+            ) (attrsToList config.dimensionTypes)
+            ++ concatMap (
+              p: map (q: { ${p.name}."dimension/${q.name}.json" = q.value; }) (attrsToList p.value)
+            ) (attrsToList config.dimensions)
           );
           config.tags = mkMerge (
             concatMap (
